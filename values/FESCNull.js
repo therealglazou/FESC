e7c1d83f822547467faecce3a2946a4d3d415968
Daniel Glazman <daniel@privowny.com>
2018-11-10 14:03:16 +0100

    context for null

---
+++ values/FESCNull.js
@@ -0,0 +1,31 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/*
+ * The Original Code is FESC, the Front-End SCSS Compiler.
+ * Original author: Daniel Glazman
+ */
+
+/**
+ * Class for SCSS null
+ */
+export class FESCNull {
+
+  /**
+   * @constructor
+   */
+  constructor() {
+    this.type = FESCValue.NULL_VALUE;
+
+  }
+
+  /**
+   * Stringifier
+   *
+   * @return {string}
+   */
+  toString() {
+    return "null";
+  }
+}
